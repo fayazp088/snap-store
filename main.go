fd5af4cc5dab1e74afedc6953f0856e39a0f5f5f
fayaz <fayazp088@gmail.com>
2023-11-19 16:15:22 +0530

    adding the templates

--- main.go
+++ main.go
@@ -1,61 +1,79 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/go-chi/chi/v5"
 )
 
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-
-	fmt.Println("Raw path: ", r.URL.RawPath)
-	fmt.Println("Route Path: ", r.URL.Path)
-
-	switch path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		http.Error(w, "Page Not Found!!!", http.StatusNotFound)
-	}
-}
-
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h1>Hello Home!!</h1>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	executeTemplate(w, filepath.Join("templates", "home.gohtml"))
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h1>Contact Page !!</h1>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	executeTemplate(w, filepath.Join("templates", "contact.gohtml"))
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, `<h1>FAQ Page</h1>
-<ul> <li>
-    <b>Is there a free version?</b>
+	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
+}
 
-	 Yes! We offer a free trial for 30 days on any paid plans.
-  </li>
-  <li>
-    <b>What are your support hours?</b>
-    We have support staff answering emails 24/7, though response
-    times may be a bit slower on weekends.
-</li> <li>
-    <b>How do I contact support?</b>
-    Email us - <a href="mailto:support@snapstore.com">support@snapstore.com</a>
-  </li>
-</ul> `)
+type User struct {
+	Name string
+	Age  int
+	Bio  string
+}
 
+func executeTemplate(w http.ResponseWriter, path string) {
+	t, err := template.ParseFiles(path)
+
+	if err != nil {
+		log.Printf("Error while parsing teampleate")
+		http.Error(w, "Error parsing template!!!", http.StatusInternalServerError)
+		return
+	}
+
+	err = t.Execute(w, nil)
+
+	if err != nil {
+		log.Printf("Error while executing teampleate")
+		http.Error(w, "Error executing template!!!", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
-	var router Router
-	http.ListenAndServe(":3000", router)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+
+	t, err := template.ParseFiles("./test.gohtml")
+
+	if err != nil {
+		panic(err)
+	}
+
+	user := User{
+		Name: "fayaz pasha",
+		Age:  27,
+		Bio:  `<script>alert("Haha, you have been h4x0r3d!");</script>`,
+	}
+
+	err = t.Execute(os.Stdout, user)
+
+	if err != nil {
+		panic(err)
+	}
+	http.ListenAndServe(":3000", r)
 }
